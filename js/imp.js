762b15ee4b4de6afa26c06b973a78396931bf0cf
abdullah956 <abdullaharshed956@gmail.com>
2024-09-05 07:49:07 +0500

    base

---
+++ js/imp.js
@@ -0,0 +1,9 @@
+function f1() {
+  var w = document.getElementById("width").value;
+  document.getElementById("img").style.width = w + "vw";
+}
+
+function f2() {
+  var h = document.getElementById("height").value;
+  document.getElementById("img").style.height = h + "vh";
+}
